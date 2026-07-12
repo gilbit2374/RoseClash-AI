51c87821fa31617cd6295d1c64e23896d2e96fb7
0SHR1 <oshriabergel0402@gmail.com>
2026-07-12 15:43:25 +0300

    added an enum for the types of obstacles

--- mydriver.py
+++ mydriver.py
@@ -2,10 +2,21 @@
 This driver does not do any action.
 """
 
-from rose.common import obstacles, actions  # NOQA
+from rose.common import obstacles,actions
+
+import enum
+class Type(enum):
+    car = -1
+    empty = 0
+    penguin = 1
+    crack = 2
+    water = 3
+    shit = 4
 
 driver_name = "MyDriver"
 
 
 def drive(world):
     return actions.NONE
+
+
